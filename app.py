9a9660ee4d2f8cba01c37ed666a38983b992da4c
Antonio Fernando <antoniofernandodj@outlook.com>
2024-10-17 17:09:22 -0300

    feat: cors origin to angular

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 fleximongo = FlexiMongo(
     url="mongodb://localhost:27017", 
-    cors_origins=["http://127.0.0.1:5500"]
+    cors_origins=["http://127.0.0.1:5500", 'http://localhost:4200']
 )
 
 fleximongo.init_app(app)
